0a213759c132154ddcc3621d32cf0137042db79d
Fríðbjartur Miðberg <55000390+fridbjartur@users.noreply.github.com>
2019-09-27 15:09:22 +0200

    Initial commit

---
+++ webpack.config.common.js
@@ -0,0 +1,63 @@
+const glob = require('glob');
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const generateHTMLPlugins = () => glob.sync('./src/**/*.html', { ignore: './src/partials/*.html' }).map(
+  dir => new HTMLWebpackPlugin({
+    filename: path.basename(dir), // Output
+    template: dir, // Input
+    minify: {
+     removeComments: true,
+     collapseWhitespace: true,
+    },
+  }),
+);
+
+module.exports = {
+  node: {
+    fs: 'empty',
+  },
+  entry: ['./src/js/app.js', './src/style/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          interpolate: true,
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|mp4)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/static/',
+        to: './static/',
+      },
+    ]),
+    ...generateHTMLPlugins(),
+  ],
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
